4a400fb27a265132b4ada8742c7b8d7df7fdfa04
Gerson <gerson_a@hotmail.com>
2025-03-21 20:20:22 +0100

    Upd 5

--- src/vecka12/test_upgifter_12_1.py
+++ src/vecka12/test_upgifter_12_1.py
@@ -7,7 +7,6 @@ from time import sleep
 from playwright.sync_api import Page, Playwright, sync_playwright, expect
 
 
-
 def test_SkapaTabortWidget(playwright: Playwright) -> None:
     """ ========= User story 1 ======================================================
      [U1] Som en användare, vill jag skapa och ta bort widgets - timer och anteckning, så att jag
@@ -23,21 +22,40 @@ def test_SkapaTabortWidget(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+    page.on('console', lambda msg: print(msg.text()))
 
     page.goto("https://lejonmanen.github.io/timer-vue/")
+    expect(page).to_have_title(re.compile("Timer app"))
+
+    expect(page.get_by_role("button").get_by_text(re.compile("Add timer"))).to_be_visible()
+    expect(page.get_by_role("button").get_by_text(re.compile("Add note"))).to_be_visible()
+    expect(page.get_by_role("button").get_by_text(re.compile("Light"))).to_be_visible()
+    expect(page.get_by_role("button").get_by_text(re.compile("Dark"))).to_be_visible()
+    expect(page.get_by_role("button").get_by_text(re.compile("Forest"))).to_be_visible()
+    expect(page.get_by_role("button").get_by_text(re.compile("Orange"))).to_be_visible()
+
+    expect(page.get_by_role("heading").get_by_text(re.compile("Break"))).not_to_be_visible()
     page.get_by_role("button", name="Add timer").click()
+    expect(page.get_by_role("heading").get_by_text(re.compile("Break"))).to_be_visible()
 
     page.get_by_role("heading", name="Break").click()
     page.get_by_role("textbox", name="Title").fill("Gerson's break")
-    sleep(1)
+    sleep(2)
 
     page.get_by_role("button", name="Add note").click()
+    expect(page.get_by_role("heading", name="Click to change text")).to_be_visible()
     page.get_by_role("heading", name="Click to change text").click()
-    page.get_by_role("textbox", name="Description").fill("Gerson note was here")
-    sleep(1)
+    page.get_by_role("textbox", name="Description").fill("Gerson was here")
+    page.get_by_role("textbox", name="Description").press("Enter")
+    expect(page.get_by_role("heading", name="Gerson was here")).to_be_visible()
+    sleep(2)
 
     page.get_by_role("button", name="Start").click()
-    sleep(5)
+
+    textboxlocator = page.locator('.row time')
+    print("\nPRINT: ", textboxlocator.input_value())
+
+    sleep(2)
 
     page.get_by_text("🗑️").nth(1).click()
     sleep(1)
